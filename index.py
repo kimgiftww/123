75e764d8fc346417b707b24a98ab63dde51d3a18
Skunpoj Thanarojsophon <kimgiftww@gmail.com>
2021-09-24 14:36:01 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
 from flask import Flask, Response
+
+from dash import Dash
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    
-    return f"Hello {path}"
+dash_app = dash.Dash(
+        server=app,
+        routes_pathname_prefix='/dashapp/',
+        external_stylesheets=[
+            '/static/dist/css/styles.css',
+        ]
+    )
+
+dash_app.layout = html.Div(['Test'], id='dash-container')
+
+@app.route('/')
+def catch_all():
+    return dash_app.server
