1d1387b1d4570b0cb68fc911d0f53b2f373c1612
Skunpoj Thanarojsophon <kimgiftww@gmail.com>
2021-09-24 09:44:22 +0700

    Update index.py

--- index.py
+++ index.py
@@ -5,12 +5,4 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def catch_all(path):
     
-    r = render_template(
-        'index.jinja2',
-        title='Plotly Dash Flask Tutorial',
-        description='Embed Plotly Dash into your Flask applications.',
-        template='home-template',
-        body="This is a homepage served with Flask."
-    )
-    
-    return Response(r)
+    return f"Hello {path}"
